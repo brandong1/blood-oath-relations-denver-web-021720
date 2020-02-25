885cb6fdb4bb964998f686b392aec0db68bb3234
github username <github email address>
2020-02-25 09:05:01 -0700

    create new cult and add arguments to instantiation

--- tools/console.rb
+++ tools/console.rb
@@ -6,9 +6,10 @@ end
 # Insert code here to run before hitting the binding.pry
 # This is a convenient place to define variables and/or set up new object instances,
 # so they will be available to test and play around with in your console
-Cult.new("Scientology", "Aurora")
-Cult.new("One", "Denver")
-Cult.new("Two", "Littleton")
+Cult.new("Scientology", "Aurora", 1910, "We made it up!")
+Cult.new("Scientology 2", "Aurora", 1950, "We're making it up as we go!")
+Cult.new("One", "Denver", 2020, "One time.")
+Cult.new("Two", "Littleton", 2016, "Two times.")
 
 binding.pry
 
