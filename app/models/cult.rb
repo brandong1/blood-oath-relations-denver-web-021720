fa133b732227dd623a52ffe3fa9e43094e69bc40
github username <github email address>
2020-02-24 20:42:30 -0700

    add location attribute

--- app/models/cult.rb
+++ app/models/cult.rb
@@ -1,15 +1,20 @@
 class Cult 
-    attr_accessor :name
+    attr_accessor :name, :location, :founding_year, :slogan, :recruit_follower
 
     @@all = []
 
-    def initialize(name)
+    def initialize(name, location)
         @name = name
+        @location = location
         @@all << self 
     end
 
     def self.all
         @@all
+    end
+
+    def location(location)
+        @location
     end
 
 
